8dbaf8d9e96e3d1a660bacd065ce938bb00d327c
NikGer11 <n.gerasimov2018@pmg-pernik.com>
2022-02-28 10:42:27 +0200

    RechniciUpr2

---
+++ RechniciUpr2/RechniciUpr2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RechniciUpr2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> EmaiList = new Dictionary<string, string>();
+            string[] input = Console.ReadLine().Split();
+
+            while (true)
+            
+            if (input[0] == "Stop")
+            {
+                break;
+            }
+
+            if (input[0].ToLower() == "Add")
+            {
+                if (!EmaiList.ContainsKey(input[1]))
+                {
+                    EmaiList.Add(input[1], input[2]);
+                }
+
+                else
+                {
+                    EmaiList[input[1]] = input[2];
+                }
+            }
+            if (input[1].ToLower() == "Sent")
+            {
+                if (EmaiList.ContainsKey(input[1]))
+                {
+                    Console.WriteLine($"{input[1]: (emailList[input[1])}");
+                }
+                else
+                {
+                    Console.WriteLine($"Contact {input[1]} does not exist");
+                }
+            }
+            input = Console.ReadLine().Split();
+        }        
+    }
+}
+
+            
+        
+   
+
+
+                
+
+                
+           
+
+        
+    
+
